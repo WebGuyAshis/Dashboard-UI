a8fdce97d514c76f4dbe52b71766633805ce5d69
Ashis <developer.ashis99@gmail.com>
2023-12-27 02:04:47 +0530

    Created Header Component

--- src/components/Dashboard/Header.jsx
+++ src/components/Dashboard/Header.jsx
@@ -1,7 +1,34 @@
-const Header = () =>{
-    return(
-        <div className="header">
+import '../../assets/styles/header.css'
+// Importing icon Images
+import layoutImg from '../../assets/images/icons/layout.png'
+import cartImg from '../../assets/images/icons/cart.png'
+import equalizerImg from '../../assets/images/icons/equalizer.png'
+import compilingImg from '../../assets/images/icons/compiling.png'
+import searchImg from '../../assets/images/icons/search.png'
 
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header-left">
+                {/* For anything to add in left side */}
+            </div>
+
+            {/* for Right */}
+            <div className="header-right">
+                <div className="user-logo">
+                    <span>N</span>
+                </div>
+
+                <span className='logged-in-user'>Nick Thomas</span>
+
+                <div className="icons">
+                    <img src={searchImg} alt="search" />
+                    <img src={compilingImg} alt="search" />
+                    <img src={equalizerImg} alt="search" />
+                    <img src={cartImg} alt="search" />
+                    <img src={layoutImg} alt="search" />
+                </div>
+            </div>
         </div>
     )
 }
